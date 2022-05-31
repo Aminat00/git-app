cd697dc0f609ef32583cba21fdae3830000e29a9
Aminat00 <38074109+Aminat00@users.noreply.github.com>
2022-05-31 10:59:03 +0200

    I replaced default react body with my own code which include h1 and image

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import madina from './images/madina.jpg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Assalamu alaikum</h1>
+      <img src={madina}/>
     </div>
   );
 }
